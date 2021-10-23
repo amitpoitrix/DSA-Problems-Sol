ede7b05b1097d7866597d12a61e3306a6cde7c3a
amit2016 <amit.poitrix@gmail.com>
2021-10-23 21:13:42 +0530

    Added C++ sol

---
+++ PepCoding/1Beginner/2Recursion&Backtracking/17_PrintKPC.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+string codes[] = {".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+
+void printKPC(string ques, string asf){
+    // write your code here
+    // base case
+    if(ques.size() == 0){
+        cout << asf << endl;
+        return;
+    }
+
+    // rec case
+    char ch = ques[0];
+    string roq = ques.substr(1);
+
+    string charfromch = codes[ch - '0'];
+
+    for (int i = 0; i < charfromch.size(); i++){
+        char cho = charfromch[i];
+        printKPC(roq, asf + cho);
+    }
+    
+}
+
+int main(){
+    string str;
+    cin >> str;
+    printKPC(str, "");
+}
