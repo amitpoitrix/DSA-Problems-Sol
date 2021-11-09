86856073d232ec3affffff655dbcd0273718d777
amit2016 <amit.poitrix@gmail.com>
2021-11-09 10:26:40 +0530

    Added C++ Sol

--- PepCoding/1Beginner/2Recursion&Backtracking/temp.cpp
+++ PepCoding/1Beginner/2Recursion&Backtracking/temp.cpp
@@ -1,12 +1,26 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int countWays(int N, int AR[]){
+void Permute(string s, int i = 0){
+    // BASE CASE
+    if (i == s.length() - 1){
+        cout << s << endl;
+        return;
+    }
     
+    // Rec case
+    for (int j = i; j < s.length(); j++){
+        swap(s[i], s[j]);
+        Permute(s, i + 1);
+        swap(s[i], s[j]);
+    }
 }
 
-int main()
-{
- cout << endl;
- return 0;
+int main(){
+    string str;
+    cin >> str;
+    Permute(str, 0);
+    
+    return 0;
 }
