3478c7ebd50cc600632bd8acd7ef8b8c949d34b0
amit_poitrix <amit.poitrix@gmail.com>
2021-12-02 12:45:39 +0530

    Added BT Problems

--- PepCoding/1Beginner/6_Trees/tempCodeRunnerFile.cpp
+++ PepCoding/1Beginner/6_Trees/tempCodeRunnerFile.cpp
@@ -1,1 +1,18 @@
-1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    map<int, int> mp;
+    mp[1] = 2;
+    mp[2] = 4;
+
+    mp.insert({5,7});
+
+    for (auto x : mp){
+        cout << x.first << " -> " << x.second << endl;
+    }
+    
+
+    cout << endl;
+    return 0;
+}
