15e13351f1a05091ba6f21606a5aabc0b4afd847
amit_poitrix <amit.poitrix@gmail.com>
2022-01-02 13:15:02 +0530

    Added Graph

--- DSA_Practice/1Beginner/10_Graph/9_1_ShortestPathUndirectedGraph.cpp
+++ DSA_Practice/1Beginner/10_Graph/9_1_ShortestPathUndirectedGraph.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+
+#define MAX 1e9
+
 // Striver Graph Series : Shortest Path in Undirected Graph with Unit Weights
 // Here we'll return a vector having shortest distance of all vertex from source(src)
 // We'll use modified version of BFS
@@ -8,30 +11,30 @@
 class Solution{
 public:
     void shortestPath(int V, std::vector<int> adj[], int src){
-        int path[V];
+        int distTo[V];
         for (int i = 0; i < V; i++){
-            path[i] = 10000000;
+            distTo[i] = MAX;
         }
+        distTo[src] = 0;  // Making the distance of src from src as 0
 
         std::queue<int> q;
-        path[src] = 0;  // Making the distance of src from src as 0
         q.push(src);
 
         while (!q.empty()){
-            int vertex = q.front();
+            int prevNode = q.front();
             q.pop();
 
-            for (auto x : adj[vertex]){
-                if(path[vertex] + 1 < path[x]){
-                    path[x] = path[vertex] + 1;
-                    q.push(x);
+            for (auto nextNode : adj[prevNode]){
+                if(distTo[prevNode] + 1 < distTo[nextNode]){
+                    distTo[nextNode] = distTo[prevNode] + 1;
+                    q.push(nextNode);
                 }
             }
         }
         
         // Display Shortest Path
         for (int i = 0; i < V; i++){
-            std::cout << path[i] << " ";
+            std::cout << distTo[i] << " ";
         }
     }
 };
@@ -54,15 +57,3 @@ int main(){
     
     return 0;
 }
-
-// 8 10
-// 0 1
-// 0 3
-// 1 2
-// 3 4
-// 3 7
-// 4 5
-// 4 6
-// 4 7
-// 5 6
-// 6 7
