de304c2c6a87cb9eee105d7fd64e2bc6c4f77855
amitpoitrix <amit.poitrix@gmail.com>
2024-12-24 13:12:27 +0530

    Added similar questions

--- Leetcode/LeetcodeDaily/2024/12_Dec/20_ReverseOddLevelsOfBinaryTree.java
+++ Leetcode/LeetcodeDaily/2024/12_Dec/20_ReverseOddLevelsOfBinaryTree.java
@@ -123,3 +123,50 @@ class Solution2 {
         solve(left.right, right.left, level + 1);
     }
 }
+
+
+/**
+ * Similar Question:
+ * 101. Symmetric Tree
+ * Link: https://leetcode.com/problems/symmetric-tree/description/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution3 {
+    public boolean isSymmetric(TreeNode root) {
+        return solve(root.left, root.right);
+    }
+
+    private boolean solve(TreeNode left, TreeNode right) {
+        // Base Case
+        if(left == null && right == null) {
+            return true;
+        }
+
+        if((left == null && right != null) || (left != null && right == null)) {
+            return false;
+        }
+
+        // Actual case when nodes value is mismatching
+        if(left.val != right.val) {
+            return false;
+        }
+        
+        // Recursive Call
+        return solve(left.left, right.right) && solve(left.right, right.left);
+    }
+}
